2e0b62e16334811ef95cc7a4833741605cfc2ee5
JozefFlakus <monodies@me.com>
2021-06-24 10:25:46 +0200

    chore(middleware-jwt): migrated package from `marble` repository

---
+++ packages/middleware-jwt/src/jwt.util.ts
@@ -0,0 +1,16 @@
+import * as O from 'fp-ts/lib/Option';
+import { pipe } from 'fp-ts/lib/function';
+import { HttpRequest } from '@marblejs/core';
+
+const splitHeader = (header: string): string[] => header.split(' ');
+
+const getLastElement = (array: string[]): string => array[array.length - 1];
+
+export const parseAuthorizationHeader = (req: HttpRequest) =>
+  pipe(
+    O.fromNullable(req.headers.authorization),
+    O.map(splitHeader),
+    O.map(getLastElement),
+    O.getOrElse(() => ''),
+  );
+
